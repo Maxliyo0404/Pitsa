80c54658b696217afeb45f0edf18e914afcc95d7
Mahliyo <gasandigital13@gmail.com>
2026-04-17 12:44:02 +0500

    napitData  yakunlandi

--- main.js
+++ main.js
@@ -1,23 +1,27 @@
-let napitData = document.getElementById("napitData");
-let napitDatas =[
-    { id:1,  img:"./image/Fire.svg ",  name:"Акции"},
-    { id:2,  img:"./image/Pizza.svg",  name:"Пицца"},
-    { id:3,  img:"./image/Group 101.svg ", name:"Суши"},
-    { id:4,  img:"./image/Drink.svg ",  name:"Напитки"},
-    { id:5,  img:"./image/Fire.svg ",  name:"Закуски"},
-    { id:6,  img:"./image/Fire.svg ",  name:"Комбо"},
-    { id:7,  img:"./image/Fire.svg ",  name:"Десерты"},
-    { id:8,  img:"./image/Fire.svg ",  name:"Соусы"},
-]
+let napitDataElement = document.getElementById("napitData"); // Element nomi o'zgartirildi
+let napitDatas = [
+    { id: 1, img: "./image/Fire.svg", name: "Акции" },
+    { id: 2, img: "./image/Pizza.svg", name: "Пицца" },
+    { id: 3, img: "./image/Group 101.svg", name: "Суши" },
+    { id: 4, img: "./image/Drink.svg", name: "Напитки" },
+    { id: 5, img: "./image/Fire.svg", name: "Закуски" },
+    { id: 6, img: "./image/Fire.svg", name: "Комбо" },
+    { id: 7, img: "./image/Fire.svg", name: "Десерты" },
+    { id: 8, img: "./image/Fire.svg", name: "Соусы" },
+];
 
-function render(napitData) {
-  let cards = napitData.map(el => `
+function render(data, container) {
+  let cards = data.map(el => `
+    <div class="cards">
     <div class="card">
-        <img class="img"  src="${el.img}" alt="${el.name}">
+        <img class="img" src="${el.img}" alt="${el.name}">
         <p class="text">${el.name}</p>
-
+    </div>
     </div>
   `).join(""); 
-  napitData.innerHTML = cards;
+  
+  container.innerHTML = cards; 
 }
-render(napitData);
+
+
+render(napitDatas, napitDataElement);
