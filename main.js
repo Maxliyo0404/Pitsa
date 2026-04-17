af83e1d2d187f62b9d36a277ea96c0dee9669b56
Mahliyo <gasandigital13@gmail.com>
2026-04-17 11:51:08 +0500

    mjs qismi

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
 let napitka = document.getElementById("napitData");
+let napitData =[
+    { id:1,  img:"./image/Fire.svg ",  name:Акции},
+    { id:2,  img:"./image/Pizza.svg",  name:Пицца},
+    { id:3,  img:"./image/Group 101.svg ", jname:Суши},
+    { id:1,  img:"./image/Fire.svg ",  name:Напитки},
+    { id:1,  img:"./image/Fire.svg ",  name:Акции},
+    { id:1,  img:"./image/Fire.svg ",  name:Акции},
+    { id:1,  img:"./image/Fire.svg ",  name:Акции},
+]
