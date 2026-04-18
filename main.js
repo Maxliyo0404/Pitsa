63a047c3c8bb4ca1ec250537f56c96cbbb247f78
Mahliyo <gasandigital13@gmail.com>
2026-04-18 11:33:20 +0500

    Комбо

--- main.js
+++ main.js
@@ -83,14 +83,15 @@ let pizza_datassam_2 = [
    
 ];
 let pizza_datassam_3 = [
-    { id: 1, img: "./image/Pizza.svg", name: "Чикен Сладкий Чили", description: "Курица, Лук, Перец Халапеньо...", price: 399, count: 1, badge: "NEW" },
-    { id: 2, img: "./image/Rectangle 4.svg", name: "EASY PEASY огуречный", description: "Курица, Лук, Соус...", price: 549, count: 1, badge: "ХИТ" },
-    { id: 3, img: "./image/Rectangle 4 (1).svg", name: "EASY PEASY чикен а-ля", description: "Курица, Лук, Соус...", price: 249, count: 1, badge: "" },
-    { id: 4, img: "./image/Rectangle 4 (2).svg", name: "4 сезона", description: "Бекон, Ветчина, Грибы...", price: 630, count: 1, badge: "" },
-    { id: 5, img: "./image/Rectangle 4 (2).svg", name: "4 сезона", description: "Бекон, Ветчина, Грибы...", price: 630, count: 1, badge: "" },
-    { id: 6, img: "./image/Rectangle 4 (1).svg", name: "EASY PEASY чикен а-ля", description: "Курица, Лук, Соус...", price: 249, count: 1, badge: "" },
-     { id: 1, img: "./image/Pizza.svg", name: "Чикен Сладкий Чили", description: "Курица, Лук, Перец Халапеньо...", price: 399, count: 1, badge: "" },
-     { id: 2, img: "./image/Rectangle 4.svg", name: "EASY PEASY огуречный", description: "Курица, Лук, Соус...", price: 549, count: 1, badge: "" },
+    { id: 1, img: "./image/kombo1.png", name: "Филадельфия кранч", description: "Семга, рис, сыр креметто, соус унаги, креветка, авокадо, чип...", price: 475, count: 1, badge: "NEW",  style: " display: block; padding: 0; width: 100%; height: aspect-ratio: 1; object-fit: cover; border-top-left-radius: 8px; border-top-right-radius: 8px;" },
+    { id: 1, img: "./image/kombo1.png", name: "Филадельфия кранч", description: "Семга, рис, сыр креметто, соус унаги, креветка, авокадо, чип...", price: 475, count: 1, badge: "NEW",  style: " display: block; padding: 0; width: 100%; height: aspect-ratio: 1; object-fit: cover; border-top-left-radius: 8px; border-top-right-radius: 8px;" },
+    { id: 1, img: "./image/kombo1.png", name: "Филадельфия кранч", description: "Семга, рис, сыр креметто, соус унаги, креветка, авокадо, чип...", price: 475, count: 1, badge: "NEW",  style: " display: block; padding: 0; width: 100%; height: aspect-ratio: 1; object-fit: cover; border-top-left-radius: 8px; border-top-right-radius: 8px;" },
+    { id: 1, img: "./image/kombo1.png", name: "Филадельфия кранч", description: "Семга, рис, сыр креметто, соус унаги, креветка, авокадо, чип...", price: 475, count: 1, badge: "NEW",  style: " display: block; padding: 0; width: 100%; height: aspect-ratio: 1; object-fit: cover; border-top-left-radius: 8px; border-top-right-radius: 8px;" },
+    { id: 1, img: "./image/kombo1.png", name: "Филадельфия кранч", description: "Семга, рис, сыр креметто, соус унаги, креветка, авокадо, чип...", price: 475, count: 1, badge: "NEW",  style: " display: block; padding: 0; width: 100%; height: aspect-ratio: 1; object-fit: cover; border-top-left-radius: 8px; border-top-right-radius: 8px;" },
+    { id: 1, img: "./image/kombo1.png", name: "Филадельфия кранч", description: "Семга, рис, сыр креметто, соус унаги, креветка, авокадо, чип...", price: 475, count: 1, badge: "NEW",  style: " display: block; padding: 0; width: 100%; height: aspect-ratio: 1; object-fit: cover; border-top-left-radius: 8px; border-top-right-radius: 8px;" },
+    { id: 1, img: "./image/kombo1.png", name: "Филадельфия кранч", description: "Семга, рис, сыр креметто, соус унаги, креветка, авокадо, чип...", price: 475, count: 1, badge: "NEW",  style: " display: block; padding: 0; width: 100%; height: aspect-ratio: 1; object-fit: cover; border-top-left-radius: 8px; border-top-right-radius: 8px;" },
+    { id: 1, img: "./image/kombo1.png", name: "Филадельфия кранч", description: "Семга, рис, сыр креметто, соус унаги, креветка, авокадо, чип...", price: 475, count: 1, badge: "NEW",  style: " display: block; padding: 0; width: 100%; height: aspect-ratio: 1; object-fit: cover; border-top-left-radius: 8px; border-top-right-radius: 8px;" },
+   
 ];
 
 let result = [...pizza_data, ...pizza_datas, ...pizza_datassa, ...pizza_datassam, ...pizza_datassam_1, ...pizza_datassam_2, ...pizza_datassam_3];
